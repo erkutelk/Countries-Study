3c058ab22069412449a1fe992985628127bbad0c
Erkut Elik <erkutelik@gmail.com>
2023-08-25 14:44:30 +0300

    Buton Hatası: Ülkeler göre bilgiler getirmek istiyorum fakat çalışmıyor

---
+++ javaDeneme.js
@@ -0,0 +1,54 @@
+
+document.querySelector("#btnSearchl").addEventListener("click", function(){
+    console.log('Butona bastım');
+    var input=document.querySelector('#search').value;
+    FetchIslemleri(input)
+});
+
+// FetchIslemleri('Turkey')
+function FetchIslemleri(country) {
+    fetch('https://restcountries.com/v3.1/name/' + country)
+    .then((response) => response.json())
+    .then((data) => {
+        oneDeger(data[0]);
+        const neighbors = data[0].borders.toString();
+        Komsular(neighbors);
+    });
+}
+
+function oneDeger(data) {
+    var deger = document.querySelector('article');
+    var resim = `
+    <div class="col-3" id="resim">
+    <h4 id="ortala">Ülke</h4>
+    <img src="${data.flags.png}" id='resim'>
+
+    
+</div>
+<div class="col-9" id="resim2">
+    <h4 id="ortala">Ülke Bilgileri</h4>
+    <ul>
+        <li>
+            <h6>Başkenti</h6>
+            <p>${data.capital[0]}</p>
+        </li>
+        <li>
+            <h6>Konuşulan Diller</h6>
+            <p>${Object.values(data.languages)}</p>
+        </li>
+        <li>
+            <h6>Komşuları</h6>
+            <p>${data.borders}<p>
+        </li>
+        
+    </ul>
+</div>
+    `;
+
+    deger.insertAdjacentHTML('beforeend', resim);
+}
+
+// function Komsular(deger) {
+//     console.log(deger);
+// }
+
